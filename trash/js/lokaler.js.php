e6a01da40c2f4d2e79dfb0eee085855d00bc1544
Mattias <info@altahr.se>
2025-08-10 17:46:57 +0200

    deleted trash

--- trash/js/lokaler.js.php
+++
@@ -1,3 +0,0 @@
-function toggleFullscreen(card) {
-    card.classList.toggle('fullscreen');
-}
--- trash/js/password.js.php
+++
@@ -1,6 +0,0 @@
-$("#searchList").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#hyresLista tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-});
